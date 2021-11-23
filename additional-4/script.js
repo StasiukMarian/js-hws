120d1b7889b2b34636f11f6ca0579831f094d919
StasiukMarian <lilmaryan2001@gmail.com>
2021-11-23 12:42:03 +0200

    created

---
+++ additional-4/script.js
@@ -0,0 +1,34 @@
+// 1.- Дано натуральное число n. Выведите все числа от 1 до n.
+
+// let iterator = (i,n) => {
+//     console.log(i)
+//     i++
+//     if (i > n ){
+//         return
+//     }
+//     return iterator(i,n)
+// }
+// iterator(1,20)
+
+
+// 2.- Даны два целых числа A и В .
+// Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае.
+
+// let iterator = (a,b) =>{
+//     if (a<b ){
+//         for (let i = a  ;i <= b; i++ ) {
+//             console.log(i)
+//         }
+//         return
+//     }
+//     else if (a>b){
+//         for (let j = a; j >= b ; j--){
+//             console.log(j)
+//         }
+//         return
+//     }
+//    iterator(a,b)
+// }
+// iterator(20,10)
+
+
