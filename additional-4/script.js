269b08694e8f8d113e42b00bbb3bde360e9310be
StasiukMarian <lilmaryan2001@gmail.com>
2021-11-24 12:56:09 +0200

    added 3,4

--- additional-4/script.js
+++ additional-4/script.js
@@ -31,4 +31,53 @@
 // }
 // iterator(20,10)
 
+// 3. функція Приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
+// let switcher = (arr, count) =>{
+//    for (let i = 0 ; i <= arr.length; i++) {
+//        let index = arr[i]
+//        if (i === count) {
+//            arr[i] = arr[i + 1]
+//            arr[i + 1] = index
+//        }
+//    }
+//     console.log(arr)
+// }
+// switcher([9,8,0,4], 1)
+
+// 4.- Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
+// Двожина масиву від 2 до 100
+// const arr = [1,0,1,0,1,1,0,0,2,3,4,5,6,0,7,0,7,0];
+// let moveZeros = (arr) => {
+//     let length = arr.length;
+//     for (let i = 0; i < length; i++) {
+//         if (arr[i] === 0) {
+//             arr.push(0);
+//             arr.splice(i, 1);
+//             length--;
+//             i--;
+//         }
+//
+//     }
+//     return arr;
+// }
+// moveZeros(arr)
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
