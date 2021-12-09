9dad702fe68e890000a9de851f38fb37a427d995
StasiukMarian <lilmaryan2001@gmail.com>
2021-12-09 13:21:05 +0200

    created

---
+++ cw11/script.js
@@ -0,0 +1,41 @@
+// є масив -
+// створити під кожен об'єкт свій блок з конопкою "додати до улюблених" при натисканні на яку об'єкт потрапляє до масиву favorites
+// улюблених обраних об'єктів в локальному сховищі.
+// Створити сторніку favorites.html при переході на яку потрібно вивест в документ всіх обраних на попередньому етапі.
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+let favourites = []
+let k = 'key'
+let changePage = document.createElement('button')
+changePage.innerText = 'Перехід на сторінку фейворітс'
+let divWrap = document.createElement('div')
+divWrap.classList.add('Wrap')
+document.body.append(divWrap,changePage)
+changePage.onclick =() =>{
+    document.location.href = 'favourites.html'
+}
+for (let item of users ){
+   let div = document.createElement('div')
+    let button = document.createElement('button')
+    button.innerText = 'add to favourite '
+    divWrap.append(div)
+    div.append(button)
+    button.onclick = () =>{
+       favourites.push(item)
+        localStorage.setItem(k, (JSON.stringify(favourites)))
+    }
+}
+
+
+
